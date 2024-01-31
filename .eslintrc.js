9bc59a595c561eb57f7db0f03abdb05614457512
Vladyslav Savchyn <vsavchyn2005@gmail.com>
2024-01-30 23:00:35 -0500

    add: eslint and prettier for jsx and react

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'no-case-declarations': 'off',
+  },
+};
